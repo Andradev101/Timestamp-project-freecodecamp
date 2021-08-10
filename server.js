c9fa3d73624b2fb5aa9bbcc7c97c57d4fc09a136
Luiz Henrique Santos de Andrade <71150787+Andradev101@users.noreply.github.com>
2021-08-10 16:05:49 +0000

    Passed on all tests

--- server.js
+++ server.js
@@ -24,8 +24,51 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/', function(req, res){
+  var currentDate = new Date(); 
+  res.json({
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString()
+  })
+})
 
+app.get('/api/:date?', function(req, res){
+  var queryInput = req.params.date;  
+  
+  var date = new Date(queryInput);
+  var dateFormat = new Date(+queryInput); 
+  
+  var dateString = new Date(queryInput);
+  var dateUnix = new Date(dateString);
 
+  dateUnixFormatted = parseInt(dateUnix.getTime());
+  dateStringFormatted = dateString.toUTCString();
+
+  var dateToMilliseconds =  date.getTime();
+  var dateToUTC = date.toUTCString();
+  
+  if(date.toString() === "Invalid Date" && dateFormat.toString() === "Invalid Date" ){//check if invalid
+    res.json({
+      error: "Invalid Date"
+    })
+  }else if(queryInput.includes('-')){//check if its a date input
+    res.json({
+      unix: `${dateToMilliseconds}`,
+      utc: `${dateToUTC}`
+    });
+  }else if(queryInput.includes(' ')){//check if it has space
+    res.json({
+      unix: dateUnixFormatted, //this was the biggest problem, i had to return it as an integer but using string litteral the return
+      utc: `${dateStringFormatted}`
+    });
+  }else if(!isNaN(queryInput)){//check if is a number
+    res.json({
+      unix: dateFormat.getTime(),
+      utc: dateFormat.toUTCString()
+    })
+  }
+})
+//needs refactory, but not now LUL
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
